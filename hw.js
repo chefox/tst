08dd1a6ad5535bba6cb9b68f73e776ea4385dcb9
chefox <cheshyr@mail.ru>
2020-10-04 23:36:55 +0300

    comment removed

--- hw.js
+++ hw.js
@@ -1,23 +1,3 @@
-//const http = require('http');
-//var fs = require('fs');
-//const hostname = '0.0.0.0';
-//const port = process.env.PORT || 80;
-
-
-//fs.readFile('./client/index.html', function (err, html) {
-//	if (err) throw err;
-//const server = http.createServer((req, res) => {
-//  res.statusCode = 200;
-//  res.setHeader('Content-Type', 'text/html');
-//  res.write(html);
-//  res.end();
-//});
-
-//server.listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`);
-//});
-//});
-
 const express = require("express");
 
 var path = require("path");
